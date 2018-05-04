b59966d0ebb54011bab664198319b3087a7dbbd1
Per Friis <per.friis@friisconsult.com>
2018-05-04 13:31:48 +0200

    Initial version of Triangle type check, web and API

---
+++ Geometric.API/Controllers/GeometryController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Geometric.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class GeometryController : Controller
+    {
+        /// <summary>
+        /// Examings the triangle.
+        /// </summary>
+        /// <returns>the triangle type (right, acute, abtuse)</returns>
+        /// <param name="A">A. length of A</param>
+        /// <param name="B">B. length of B</param>
+        /// <param name="C">C. length of C, C must be the longste side</param>
+        [HttpGet]
+        public string ExamingTriangle(float A, float B, float C) {
+
+            if (C != Math.Max(C,Math.Max(B,A))) {
+                return "make sure that C is the longste side";
+            }
+
+            TriangleType type = TriangleType.unknown;
+
+            var A2B2 = Math.Pow(A, 2) + Math.Pow(B, 2);
+
+            var C2 = Math.Pow(C, 2);
+
+            if (C2 == A2B2) {
+                type = TriangleType.right;
+            } else if (C2 < A2B2) {
+                type = TriangleType.acute;
+            } else if (C2 > A2B2) {
+                type = TriangleType.obtuse;
+            }
+
+            return $"the triangle is a {type.ToString()} triangle A={A}, B={B}, C={C}";
+        }
+    }
+
+
+    /// <summary>
+    /// Triangle type.
+    /// </summary>
+    enum TriangleType
+    {
+        unknown,
+        right,
+        acute,
+        obtuse
+    }
+}
---
+++ Geometric.API/Startup.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.PlatformAbstractions;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace Geometric.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            // setup Swagger
+            services.AddSwaggerGen(options =>
+            {
+                
+
+                options.DescribeAllEnumsAsStrings();
+                options.DescribeAllParametersInCamelCase();
+                options.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info
+                {
+                    Title = "Geometry services",
+                    Description = "This is a showcase, created by Per Friis on request for code show",
+                    Contact = new Contact
+                    {
+                        Email = "per.friis@friisconsult.com",
+                        Name = "Per Friis Consult aps",
+                        Url = "https://friisconsult.com"
+                    },
+                    TermsOfService = "This is a completely open services, it has no real purpose other thatn code showcase",
+                    Version = "1.0"
+                });
+
+                var filePath = PlatformServices.Default.Application.ApplicationBasePath + "Geometric.API.xml";
+                options.IncludeXmlComments(filePath);
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Walk The Talk - Server");
+            });
+
+            app.UseMvc();
+        }
+    }
+}
